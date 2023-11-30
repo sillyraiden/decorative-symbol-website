1eb5308d04923f9e1a9a0df39190aacc39c511df
raiden <124207037+sillyraiden@users.noreply.github.com>
2023-11-30 23:53:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function copyToClipboard() {
+    var copyText = document.getElementById("textInput");
+
+    navigator.clipboard.writeText(copyText.value)
+        .then(() => {
+            alert("Copied: " + copyText.value);
+            console.log("Text copied to clipboard");
+        })
+        .catch(err => {
+            console.error("Unable to copy text to clipboard", err);
+        });
+}
