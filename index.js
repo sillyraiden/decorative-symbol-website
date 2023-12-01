1b1f3556446be16aa9037b22f95d9623975fd425
raiden <124207037+sillyraiden@users.noreply.github.com>
2023-12-01 11:42:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,26 +10,3 @@ function copyToClipboard() {
             console.error("Unable to copy text to clipboard", err);
         });
 }
-
-document.getElementById("box").addEventListener("click", function(event) {
-    if (event.target.tagName === "DIV") {
-        copySymbol(event.target.textContent.trim());
-    }
-});
-
-function copySymbol(symbol) {
-    document.getElementById("textInput").value = symbol;
-}
-
-function copyToClipboard() {
-    var copyText = document.getElementById("textInput");
-
-    navigator.clipboard.writeText(copyText.value)
-        .then(() => {
-            alert("Copied: " + copyText.value);
-            console.log("Text copied to clipboard");
-        })
-        .catch(err => {
-            console.error("Unable to copy text to clipboard", err);
-        });
-}
