78c678a696d441399a671c8c5f13e8e1d357d48b
raiden <124207037+sillyraiden@users.noreply.github.com>
2023-12-01 11:48:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var symbolBoxes = document.querySelectorAll("#box div");
+    symbolBoxes.forEach(function (box) {
+        box.addEventListener("click", function () {
+            copySymbol(box.textContent.trim());
+        });
+    });
+});
+
+function copySymbol(symbol) {
+    document.getElementById("textInput").value = symbol;
+}
+
 function copyToClipboard() {
     var copyText = document.getElementById("textInput");
 
